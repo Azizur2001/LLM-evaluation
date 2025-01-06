94b9a28f001794f9f67a6b680aecee7aaa27c0b8
Azizur Rahman <azizur.rahman59@qmail.cuny.edu>
2025-01-05 19:05:51 -0500

    added logs

--- src/components/ModelResponsePanel.js
+++ src/components/ModelResponsePanel.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import { Paper, Typography, CircularProgress, Box } from "@mui/material";
-
-// const ModelResponsePanel = ({ modelName, response, isLoading }) => {
-//   return (
-//     <Paper
-//       elevation={3}
-//       style={{ padding: "16px", height: "200px", position: "relative" }}
-//     >
-//       <Typography variant="h6" gutterBottom>
-//         {modelName}
-//       </Typography>
-//       {isLoading ? (
-//         <Box
-//           display="flex"
-//           justifyContent="center"
-//           alignItems="center"
-//           style={{ height: "100%" }}
-//         >
-//           <CircularProgress />
-//         </Box>
-//       ) : response ? (
-//         <Typography>{response}</Typography>
-//       ) : (
-//         <Typography style={{ color: "#aaa" }}>No response yet</Typography>
-//       )}
-//     </Paper>
-//   );
-// };
-
-// export default ModelResponsePanel;
-
 import React from "react";
 import { Paper, Typography, CircularProgress, Box } from "@mui/material";
 const ModelResponsePanel = ({ modelName, response, isLoading }) => {
